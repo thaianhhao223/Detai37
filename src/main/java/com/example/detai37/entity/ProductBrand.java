35f9b95cd9fec1b39512c35c3b722e804cc61ce2
henry.thai <henry.thai@alpaca.vn>
2022-05-17 00:05:59 +0700

    update product brand

--- src/main/java/com/example/detai37/entity/ProductBrand.java
+++ src/main/java/com/example/detai37/entity/ProductBrand.java
@@ -1,9 +1,11 @@
 package com.example.detai37.entity;
 
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
@@ -14,6 +16,8 @@ import javax.persistence.Id;
 @Builder
 public class ProductBrand {
     @Id
+    @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
     private String id;
     @Column(columnDefinition = "NVarchar(255)")
     private String name;
